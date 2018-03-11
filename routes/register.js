d26483ea27d6b2f995c30a6f71b6f55cde7580bb
Taylor <thulsmans.2994@me.com>
2018-03-11 18:19:54 +0000

    registrations now create a session cookie

--- routes/register.js
+++ routes/register.js
@@ -5,36 +5,39 @@ const path = require('path');
 const bcrypt = require('bcrypt');
 const router = express.Router();
 
-
-module.exports = (knex) => {
-
-/*  router.get('/', function(req, res, next){
-   res.sendFile('/');
-});*/
-
-router.post('/register', (req, res, next) => {
-
-  knex.select('email').from('users').then(function(emails) {
-    if (emails.length) {
-       return next(err);
-    } else {
-
-      bcrypt.hash(req.body.password, 9, (err, hash) => {
-        if (err) return next(err);
-        const newUser = {name: req.body.username, email: req.body.email};
-        newUser.password = hash;
-        return knex('users').insert(newUser).then(function() {
-           res.redirect('http://localhost:3000')
-           return
-        })
-      })
-    }
-  })
-
-})
-  return router
+function generateRandomString() {
+  // https://stackoverflow.com/questions/10726909/random-alpha-numeric-string-in-javascript
+  return Math.random().toString(36).slice(7);
 }
 
 
+module.exports = (knex) => {
 
+  /*  router.get('/', function(req, res, next){
+     res.sendFile('/');
+  });*/
 
+  router.post('/register', (req, res) => {
+    const randomId = generateRandomString();
+    knex.select('email').from('users').where('email', req.body.email).then(function(emails) {
+      if (emails.length) {
+        res.send('email already in use!');
+        return
+      } else {
+
+        bcrypt.hash(req.body.password, 9, (err, hash) => {
+          if (err) return err;
+          const newUser = { name: req.body.username, email: req.body.email };
+          newUser.password = hash;
+          return knex('users').insert(newUser).then(function() {
+            req.session.user_id = randomId;
+            res.redirect('http://localhost:3000')
+            return
+          })
+        })
+      }
+    })
+
+  })
+  return router
+}
