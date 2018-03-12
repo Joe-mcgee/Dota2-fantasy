1cccfa4296ab4e817ad054653c51e1fbb0aa6bb1
Taylor <thulsmans.2994@me.com>
2018-03-12 03:05:14 +0000

    changed over api to sports trader

--- server.js
+++ server.js
@@ -4,24 +4,19 @@ const express = require('express');
 const knexConfig = require('./knexfile');
 const knex = require('knex')(knexConfig[ENV]);
 const bodyParser = require('body-parser');
-<<<<<<< HEAD
-const request = require('request')
-const strint = require('./strint/strint.js')
-const Dota2Api = require('dota2-api');
 
-=======
 const request = require('request-promise');
 const strint = require('./strint/strint.js');
 const Dota2Api = require('dota2-api');
-const da = Dota2Api.create('FF967EC4968D206F9FA1485AC5F6E162');
->>>>>>> 6ecaeab565de11d3df211a391a2a2686c9de4c9a
+const da = Dota2Api.create('FF967EC4968D206F9FA1485AC5F6E162', 570);
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieSession({
   name: 'session',
   keys: ['key1', 'key2']
-}))
+}));
 const port = process.env.PORT || 5000;
 
 //Resource Routes
@@ -33,35 +28,19 @@ app.use('/', registerRoutes(knex));
 
 
 app.get('/api/hello', (req, res) => {
-
-  // const da = Dota2Api.create('FF967EC4968D206F9FA1485AC5F6E162', 570)
-  // const oprtions = {game_mode: 1};
-  //   da.getMatchHistory('3719509349').then((result) => {
-  //     console.log(result);
-  // }, (errorResponseStatusText) => {
-  //     console.log(errorResponseStatusText);
-  // });
-
-  //let BigSteam64;
   request.get({
-    "uri": `https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?match_id=3719509349&key=FF967EC4968D206F9FA1485AC5F6E162`
+    "uri": `http://api.sportradar.us/dota2-t1/en/schedules/2018-03-08/schedule.json?api_key=${process.env.SPORT_TRADER_KEY}`
   }).then((response) => {
-/*    console.log(1, response)
-    console.log('two', response.result)
-    console.log('three', response.result.players)
-    console.log('four', response.result.players[0])
-    console.log('five', response.result.players[0].account_id)*/
-    const data = JSON.parse(response)
-    const steam32 = String(data.result.players[0].account_id);
-    console.log(steam32)
-    let BigSteam64 = String(strint.add("76561197960265728", '115090068'));
-    console.log(BigSteam64);
+    const data = JSON.parse(response);
+    res.send(data)
+
     return request.get({
-       "uri": `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=FF967EC4968D206F9FA1485AC5F6E162&steamids=${BigSteam64}`
-     }).pipe(res)
+      "uri": `http://api.sportradar.us/dota2-t1/en/schedules/2018-03-08/schedule.json?api_key=${process.env.SPORT_TRADER_KEY}`
+    }).pipe(res)
   });
 
 });
+
 
 
 
