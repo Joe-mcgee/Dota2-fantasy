6ecaeab565de11d3df211a391a2a2686c9de4c9a
Taylor <thulsmans.2994@me.com>
2018-03-12 00:23:09 +0000

    added api chain for player in match

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const ENV = process.env.ENV || 'development';
-const cookieSession = require('cookie-session')
+const cookieSession = require('cookie-session');
 const express = require('express');
 const knexConfig = require('./knexfile');
 const knex = require('knex')(knexConfig[ENV]);
 const bodyParser = require('body-parser');
-const request = require('request')
-const BigNumber = require('bignumber.js');
-const strint = require('./strint/strint.js')
-
+const request = require('request-promise');
+const strint = require('./strint/strint.js');
+const Dota2Api = require('dota2-api');
+const da = Dota2Api.create('FF967EC4968D206F9FA1485AC5F6E162');
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieSession({
   name: 'session',
   keys: ['key1', 'key2']
@@ -26,13 +26,28 @@ app.use('/', registerRoutes(knex));
 
 
 app.get('/api/hello', (req, res) => {
-  const BigSteam64 = strint.add("76561198075355796", "0")
-  const steam64 = '76561198075355796'
-  console.log(`something=${BigSteam64}`)
-  request({
-    "uri": `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=FF967EC4968D206F9FA1485AC5F6E162&steamids=${BigSteam64}`
-  }).pipe(res);
+  //let BigSteam64;
+  request.get({
+    "uri": `https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?match_id=3719509349&key=FF967EC4968D206F9FA1485AC5F6E162`
+  }).then((response) => {
+/*    console.log(1, response)
+    console.log('two', response.result)
+    console.log('three', response.result.players)
+    console.log('four', response.result.players[0])
+    console.log('five', response.result.players[0].account_id)*/
+    const data = JSON.parse(response)
+    const steam32 = String(data.result.players[0].account_id);
+    console.log(steam32)
+    let BigSteam64 = String(strint.add("76561197960265728", '115090068'));
+    console.log(BigSteam64);
+    return request.get({
+       "uri": `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=FF967EC4968D206F9FA1485AC5F6E162&steamids=${BigSteam64}`
+     }).pipe(res)
+  });
 });
+
+
+
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
