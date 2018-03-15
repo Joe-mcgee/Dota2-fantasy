08af1e5e057391cf2e5142c25017d89fa21a49dd
boxuanlu <boxuan@ualberta.ca>
2018-03-14 23:54:36 -0600

    start working on bootstrap

--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -1,6 +1,21 @@
 import React, {Component} from 'react';
 import Authenticator from './Authenticator.jsx';
 
+const styleA = {
+  backgroundColor: 'rgb(14,14,14)'
+}
+
+const styleB = {
+  width: '203px',
+  margin: '-50px -50px -50px -30px'
+}
+
+const styleC = {
+  backgroundColor: '#fca311',
+  borderColor:'#ffba53',
+  height:'46px'
+}
+
 /*
 * Child Component
 */
@@ -9,14 +24,15 @@ class NavBar extends Component {
   render() {
 
     return (
+      <div>
+        <nav className="navbar navbar-light navbar-expand-md navigation-clean-button" style={styleA}>
+            <div className="container"><a href="#" className="navbar-brand"><img src="/fantasydota-02_1024.png" style={styleB} /></a><button data-toggle="collapse" data-target="#navcol-1" className="navbar-toggler"><span className="sr-only">Toggle navigation</span><span className="navbar-toggler-icon"></span></button>
+                <div
+                    className="collapse navbar-collapse" id="navcol-1">
+                    <ul className="nav navbar-nav mr-auto"></ul><span className="navbar-text actions"><button className="btn btn-primary" type="button" style={styleC}>Log Out</button></span></div>
+            </div>
+        </nav>
 
-      <div className="p-3 mb-2 bg-warning text-dark">
-      <nav className="navbar navbar-light bg-light">
-      <div className="p-3 mb-2 bg-warning text-dark">
-        <a href="/" className="navbar-brand">Future Name</a>
-        <Authenticator />
-</div>
-      </nav>
       </div>
       );
   }
--- client/src/app123.jsx
+++ client/src/app123.jsx
@@ -32,16 +32,32 @@ class App extends Component {
   };*/
 
 
-
-
   render() {
     return (
       <div>
-        <NavBar />
+        <div className="container-fluid">
+          <NavBar />
+        </div>
+
+        <p>
+          {this.state.response}
+        </p>
+        <ul className="nav nav-pills nav-fill">
+          <li className="nav-item">
+           <a className="nav-link " href="#">Ready to go</a>
+          </li>
+            <li className="nav-item">
+          <a className="nav-link " href="#">Get more token</a>
+          </li>
+        </ul>
+        <button type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off">
+          back to login
+        </button>
       </div>
    );
   }
 }
+
 
 // api https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?match_id=3719509349&key=FF967EC4968D206F9FA1485AC5F6E162
 // then find account id then    STEAMID32 + 76561197960265728 = STEAMID64  http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=XXXXXXXXXXXXXXXXXXXXXXX&steamids=STEAMID64
