838704f5c1f1b3425bf140fc0921183c54dc41d9
Taylor <thulsmans.2994@me.com>
2018-03-19 22:04:21 +0000

    added betting abi and such

--- betting/compile.js
+++ betting/compile.js
@@ -4,5 +4,7 @@ const solc = require('solc');
 
 const bettingPath = path.resolve(__dirname, 'contracts', 'Betting.sol');
 const source = fs.readFileSync(bettingPath, 'utf-8');
-
+console.log(source)
 module.exports = solc.compile(source, 1).contracts[':Betting'];
+
+
