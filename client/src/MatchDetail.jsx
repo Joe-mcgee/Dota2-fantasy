0043ac8bb6bbddca603bd280a2a41936beeadc10
Jesse Roy-Cote <jesseroyc@gmail.com>
2018-03-22 13:37:03 -0600

    added final touch styling

--- client/src/MatchDetail.jsx
+++ client/src/MatchDetail.jsx
@@ -101,7 +101,7 @@ render() {
     return (
 
 
-      <div className="card-body">
+      <div className="card-body" style={style.body}>
 
         {/* Timer element */}
         <h4 className="text-center card-title" style={style.timer}>
@@ -216,35 +216,139 @@ render() {
             </div>
           </div>
         </div>
+        <div className="card-footer text-center" style={style.body}>
+
+          <div style={style.info}>
+            <h6>
+              <strong>
+                What is this?
+              </strong>
+            </h6>
+            <h6 style={style.description}>
+              This is a 100% safe, secure, and trust-less friendly competition.
+            </h6>
+          </div>
+
+          <div style={style.info}>
+            <h6>
+              <strong>
+              Is it safe? Why should I trust you?
+              </strong>
+            </h6>
+            <h6 style={style.description}>
+              Yes, 100%. You dont have to trust us! The smart contract code that processes bets and pays winners is 100% public. Read it yourself. This is the magic of Ethereum.
+            </h6>
+          </div>
+
+          <div style={style.info}>
+            <h6>
+              <strong>
+              How much ETH can I win?
+              </strong>
+            </h6>
+            <p style={style.description}>
+              Winners will get their money back plus a chunk of the losing team's pot (minus our 10% commission) that's proportional to the amount each person contributed. The site will display your expected payout.
+            </p>
+          </div>
+
+          <div style={style.info}>
+            <h6>
+              <strong>
+              When will winners be paid out?
+              </strong>
+            </h6>
+            <h6 style={style.description}>
+              Winners will be paid out as soon as the winner is available on Wolfram Alpha. Likely as soon as the game is over.
+            </h6>
+          </div>
+
+          <div style={style.info}>
+            <h6>
+              <strong>
+              Can our money get stuck in here?
+              </strong>
+            </h6>
+            <h6 style={style.description}>
+              No, if the payouts aren't automatically triggered after a week (i.e. Wolfram Alpha goes offline), the contract will return everyone's bets.
+            </h6>
+          </div>
+
+          <div style={style.info}>
+            <h6>
+              <strong>
+              How is the game result communicated?
+              </strong>
+            </h6>
+            <h6 style={style.description}>
+              We don't decide the winner, Wolfram Alpha does. We are using Oraclize.it to communicate the game results with the smart contract, with a Wolfram Alpha query for "Super Bowl LII Winner". See the smart contract.
+            </h6>
+          </div>
+
+          <div style={style.info}>
+            <h6>
+              <strong>
+              How are you guys making money?
+              </strong>
+            </h6>
+            <h6 style={style.description}>
+              It costs nothing to place a bet. We will be taking a 10% commission from the prize pool.
+            </h6>
+          </div>
+
+          <div style={style.info}>
+            <h6>
+              <strong>
+              How do I contact you guys?
+              </strong>
+            </h6>
+            <h6 style={style.description}>
+              You can message us through this link.
+            </h6>
+          </div>
+
+        </div>
       </div>
     );
    }
 }
 
 const style = {
+  info: {
+    margin: '4em 0em 4em 0em',
+    width: '42em',
+    justifyContent: 'center',
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  },
+  body: {
+    margin: '1em 0em 0em 0em',
+    justifyContent: 'center'
+  },
   timer: {
     fontSize:'2em',
   },
 
   teamLiquidBackground: {
+    padding: '2em 0em 0em 0em',
     float: 'right',
     width: '22em',
     backgroundColor: 'rgb(221, 234, 255)'
   },
 
   otherTeamBackground: {
+    padding: '2em 0em 0em 0em',
     float: 'left',
     width: '22em',
     backgroundColor: 'rgb(255, 234, 234)'
   },
 
   teamLiquidFooter: {
-    height:'70px',
+    height:'75px',
     backgroundColor:'rgb(40, 104, 255)'
   },
 
   otherTeamFooter: {
-    height:'70px',
+    height:'75px',
     backgroundColor:'rgb(255, 40, 40)'
   },
 
